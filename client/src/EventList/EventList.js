6be5263aa683a8b25afebeb4107143c7da7d6076
Carlotta <carlotta.clauter@gmail.com>
2021-12-13 16:38:22 +0100

    filter future events

--- client/src/EventList/EventList.js
+++ client/src/EventList/EventList.js
@@ -4,13 +4,15 @@ import FirstEvent from '../FirstEvent/FirstEvent';
 
 function EventList({ events }) {
 
-  events.sort((a, b) => {
+  const futureEvents = events.filter(event => new Date(event.date) > Date.now())
+
+  futureEvents.sort((a, b) => {
     return new Date(a.date) - new Date(b.date)
   })
 
   return (
-    events.length ?
-      events.map(evnt => evnt === events[0] ? <FirstEvent evnt={evnt} key={evnt._id}></FirstEvent> :
+    futureEvents.length ?
+      futureEvents.map(evnt => evnt === futureEvents[0] ? <FirstEvent evnt={evnt} key={evnt._id}></FirstEvent> :
         <EventItem evnt={evnt} key={evnt._id}></EventItem>)
       : <p>No events planned!</p>
   )
